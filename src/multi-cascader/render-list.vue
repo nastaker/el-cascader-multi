1fda13b14887f0e9682accdaa6edb260d52e2bfd
nastaker <nastaker@163.com>
2019-04-16 17:24:47 +0800

    Update render-list.vue

--- src/multi-cascader/render-list.vue
+++ src/multi-cascader/render-list.vue
@@ -3,12 +3,11 @@
     <li class="li-style"
       v-for="(node, nodeIndex) in list"
       :key="getKey(node)"
-      @click="handleClick(node, nodeIndex, level)"
       :class="{'active-li': activeList[level - 1] === node.id}"
       @mouseenter="handleMouseEnter(node, nodeIndex, level)"
     >
       <el-checkbox @change="handleCheck($event, node)" v-model="node.checked" :disabled="node.disabled"></el-checkbox>
-      <span style="margin-left:5px">{{node[labelKey]}}</span>
+      <span @click="handleClick(node, nodeIndex, level)" style="margin-left:5px">{{node[labelKey]}}</span>
       <i v-if="node.childNodes && node.childNodes.length > 0" class="li-label-icon el-icon-arrow-right"></i>
     </li>
   </ul>
